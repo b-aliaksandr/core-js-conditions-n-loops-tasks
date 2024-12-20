febc11b9a540258b6d466117a0e3282f6643527e
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2024-12-20 18:52:22 +0300

    feat: implement isIsoscelesTriangle fn

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -105,8 +105,18 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a === 0 || b === 0 || c === 0) return false;
+  if (a === b) {
+    return a + b >= c;
+  }
+  if (a === c) {
+    return a + c >= b;
+  }
+  if (b === c) {
+    return b + c >= a;
+  }
+  return false;
 }
 
 /**
