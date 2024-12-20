bbd195516ca95336ee5a9311ae7bedd512723e9f
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2024-12-20 16:32:01 +0300

    feat: implement getMaxNumber fn

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,12 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let max = a > b ? a : b;
+  if (max < c) {
+    max = c;
+  }
+  return max;
 }
 
 /**
